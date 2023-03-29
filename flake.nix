a1f79bb6cd13a54ba1982fdcfc196ffbe222c752
bonjour <bonjour@tarneo.fr>
2023-03-29 16:59:11 +0200

    Correct syntax error (unexpected let)

--- flake.nix
+++ flake.nix
@@ -5,20 +5,19 @@
     nixpkgs.url = "github.com:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      # if you want absolutely proprietary packages
+      # config.allowUnfree = true;
+    };
+  in {
+    nixosConfigurations = {
+      server-test = lib.nixosSystem {
         inherit system;
-	# if you want absolutely proprietary packages
-        # config.allowUnfree = true;
-      };
-    in {
-      nixosConfigurations = {
-        server-test = lib.nixosSystem {
-	  inherit system;
-	  modules = [ ./configuration.nix ];
-	};
+        modules = [ ./configuration.nix ];
       };
     };
   };
