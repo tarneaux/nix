cc2b2706890d2afd0ad5de5eb9d6b41772e3d426
bonjour <bonjour@tarneo.fr>
2023-03-29 17:23:13 +0200

    Add home manager

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -16,9 +20,20 @@
     lib = nixpkgs.lib;
   in {
     nixosConfigurations = {
-      server-test = lib.nixosSystem {
+      server-test = let
+        main-user-name = "max";
+      in lib.nixosSystem {
         inherit system;
-        modules = [ ./configuration.nix ];
+        modules = [
+	  ./configuration.nix
+	  home-manager.nixosModules.home-manager {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.${main-user-name} = {
+	      imports = [ ./server-test-home.nix ];
+	    };
+	  }
+	];
       };
     };
   };
