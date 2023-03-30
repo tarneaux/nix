1ee5905dcdc3827dd2bf8902679c6b1320d62bb2
tarneaux <tarneo@tarneo.fr>
2023-03-30 19:02:18 +0200

    Add git config and aliases

--- hosts/server/home-manager.nix
+++ hosts/server/home-manager.nix
@@ -9,6 +9,26 @@
     stateVersion = "22.11";
   };
 
+  programs.git = {
+    enable = true;
+    userName = "server";
+    userEmail = "server@renn.es";
+    aliases = {
+      ga = "git add";
+      gap = "git add --patch";
+      gc = "git commit";
+      gcm = "git commit -m";
+      gca = "git commit -a";
+      gcam = "git commit -am";
+      gp = "git push";
+      gs = "git status";
+      gl = "git log --decorate --oneline --graph";
+      gd = "git diff";
+      gb = "git branch";
+      gco = "git checkout";
+    };
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
