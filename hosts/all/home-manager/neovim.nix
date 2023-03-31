541d0586d20e12690c925ceda3cb5972fb58eaab
tarneaux <tarneo@tarneo.fr>
2023-03-31 18:26:49 +0200

    Add nvim LSP and a few config options

--- hosts/all/home-manager/neovim.nix
+++ hosts/all/home-manager/neovim.nix
@@ -5,6 +5,27 @@
   defaultEditor = true;
   viAlias = true;
   vimAlias = true;
+  extraLuaConfig = "
+  vim.opt.rnu = true -- Show line numbers
+  vim.opt.signcolumn = 'number' -- Show line numbers in the sign column
+  vim.opt.shiftwidth = 4 -- Indentation width
+  vim.opt.expandtab = true -- Use spaces instead of tabs
+  vim.opt.smarttab = true -- Use smart tabbing
+  vim.opt.linebreak = true -- Wrap long lines
+  vim.opt.swapfile = false -- Disable those annoying swap files
+  vim.opt.clipboard = 'unnamedplus' -- Copy to system clipboard
+  vim.opt.undofile = true -- Enable undo history
+  vim.opt.undodir = '~/.local/share/nvim-undodir' -- Set undo history directory
+  vim.opt.scrolloff = 1 -- Keep 1 line above and below the cursor
+  vim.opt.autoread = true -- Reload file if it changes on disk
+
+  -- Disable background color for sign column
+  vim.cmd.highlight('SignColumn', 'guibg=NONE')
+  -- same for gitgutter signs
+  vim.cmd.highlight('GitGutterAdd', 'guibg=NONE')
+  vim.cmd.highlight('GitGutterChange', 'guibg=NONE')
+  vim.cmd.highlight('GitGutterDelete', 'guibg=NONE')
+  ";
   plugins = with pkgs.vimPlugins; [
     # Gruvbox colorscheme
     {
@@ -52,5 +73,18 @@
     {
       plugin = vim-automkdir;
     }
+    # LSP = language server protocol. Used for autocompletion, go to definition, etc.
+    {
+      plugin = nvim-lspconfig;
+      config = "
+      require('lspconfig').pyright.setup{}
+      require('lspconfig').bashls.setup{}
+      require('lspconfig').rnix.setup{}
+      ";
+      type = "lua";
+    }
+  ];
+  extraPackages = with pkgs; [
+    pyright nodePackages.bash-language-server rnix-lsp # LSP servers
   ];
 }
