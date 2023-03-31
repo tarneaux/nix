adb0f1451b368ad003f6009f4ffa0978b71974ef
tarneaux <tarneo@tarneo.fr>
2023-03-31 17:08:42 +0200

    Enable vi and vim aliases for neovim

--- hosts/all/home-manager/neovim.nix
+++ hosts/all/home-manager/neovim.nix
@@ -3,6 +3,8 @@
 {
   enable = true;
   defaultEditor = true;
+  viAlias = true;
+  vimAlias = true;
   plugins = with pkgs.vimPlugins; [
     { plugin = gruvbox;
       config = "colorscheme gruvbox";
