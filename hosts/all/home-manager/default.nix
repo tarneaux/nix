124064ab3b9953b7d49357a0ae7720450f27a4bb
tarneaux <tarneo@tarneo.fr>
2023-03-31 19:46:06 +0200

    Add zsh aliases and some other things

--- hosts/all/home-manager/default.nix
+++ hosts/all/home-manager/default.nix
@@ -10,13 +10,19 @@
     stateVersion = "22.11";
     packages = with pkgs; [
       bat # cat clone with syntax highlighting and Git integration. Written in Rust (UwU)
+      fd # simple, fast and user-friendly alternative to find
+      fzf # command-line fuzzy finder
+      git # you don't need me to explain what it is, do you?
+      ripgrep # grep clone with an eye on speed
+      trash-cli # command-line interface to the freedesktop.org Trash
+      exa # ls clone with colors and icons. Written in Rust (UwU) too
     ];
   };
   programs.home-manager.enable = true;
 
 
+  programs.zsh = import ./zsh.nix { inherit pkgs lib; };
   programs.git = import ./git.nix { inherit pkgs; };
-  programs.zsh = import ./zsh.nix { inherit pkgs; };
   programs.neovim = import ./neovim.nix { inherit pkgs; };
   programs.starship = import ./starship.nix { inherit pkgs lib; };
 }
--- hosts/all/home-manager/zsh.nix
+++ hosts/all/home-manager/zsh.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   enable = true;
@@ -11,4 +11,36 @@
     ignoreDups = true;
     share = true;
   };
+
+  initExtra = lib.strings.concatStringsSep "\n" [
+    # General aliases
+    "alias t='trash'"
+    "alias v='nvim'"
+    "alias g='git'"
+
+    # Confirm irrevocable commands
+    "alias cp='cp -i'"
+    "alias mv='mv -i'"
+    "alias rm='rm -i'"
+
+    # Shadowed system commands
+    "alias ls='exa --color=auto --icons --group-directories-first'"
+    "alias ll='ls -l'"
+    "alias la='ls -la'"
+    "alias tree='ls --tree'"
+
+    # Git aliases
+    "alias ga='git add'"
+    "alias gap='git add --patch'"
+    "alias gc='git commit'"
+    "alias gca='git commit --amend'"
+    "alias gcm='git commit --message'"
+    "alias gcam='git commit --amend --message'"
+    "alias gp='git push'"
+    "alias gs='git status'"
+    "alias gd='git diff'"
+    "alias gb='git branch'"
+    "alias gco='git checkout'"
+    "alias gl='git log --decorate --oneline --graph'"
+  ];
 }
