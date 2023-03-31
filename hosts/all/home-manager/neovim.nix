01374a148553d0b1aa8b1bcc75a731f9dede8d2d
tarneaux <tarneo@tarneo.fr>
2023-03-31 19:52:46 +0200

    Add alpha-nvim (dashboard) nvim plugin

--- hosts/all/home-manager/neovim.nix
+++ hosts/all/home-manager/neovim.nix
@@ -113,6 +113,33 @@
     {
       plugin = vim-commentary;
     }
+    # Alpha-nvim = dashboard
+    {
+      plugin = alpha-nvim;
+      config = ''
+        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+
+        local alpha = require'alpha'
+        local dashboard = require'alpha.themes.dashboard'
+        dashboard.section.buttons.val = {
+            dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+            dashboard.button( "spc f f", "  Find file" , ":Telescope find_files<CR>"),
+            dashboard.button( "spc f r", "  Recently used files" , ":Telescope oldfiles<CR>"),
+            dashboard.button( "q", "  Quit NVIM" , ":qa<CR>"),
+        }
+        local handle = io.popen('fortune calvin -s')
+        local fortune = handle:read("*a")
+        handle:close()
+        dashboard.section.header.val = fortune
+
+        dashboard.config.opts.noautocmd = true
+
+        vim.cmd[[autocmd User AlphaReady echo 'ready']]
+
+        alpha.setup(dashboard.config)
+      '';
+      type = "lua";
+    }
   ];
   extraPackages = with pkgs; [
     pyright nodePackages.bash-language-server rnix-lsp # LSP servers
