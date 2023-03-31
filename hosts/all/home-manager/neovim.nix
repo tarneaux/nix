d0d8437853f5892a9545a9f57eb6422b317b6957
tarneaux <tarneo@tarneo.fr>
2023-03-31 17:25:07 +0200

    Add nvimtree and undotree

--- hosts/all/home-manager/neovim.nix
+++ hosts/all/home-manager/neovim.nix
@@ -6,13 +6,24 @@
   viAlias = true;
   vimAlias = true;
   plugins = with pkgs.vimPlugins; [
+    # Gruvbox colorscheme
     {
       plugin = gruvbox;
       config = "colorscheme gruvbox";
     }
+    # Lualine = statusline
     {
       plugin = lualine-nvim;
       config = "lua require('lualine').setup{}";
+    }
+    # NvimTree = file explorer
+    {
+      plugin = nvim-tree-lua;
+      config = "lua require('nvim-tree').setup{}";
+    }
+    # UndoTree = undo history and easy navigation
+    {
+      plugin = undotree;
     }
   ];
 }
