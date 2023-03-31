b4413d06e0056fab16eaa17152942743ba702c85
tarneaux <tarneo@tarneo.fr>
2023-03-31 15:01:53 +0200

    Reformat hosts/all/home-manager/default.nix

--- hosts/all/home-manager/default.nix
+++ hosts/all/home-manager/default.nix
@@ -9,11 +9,9 @@
     homeDirectory = "/home/${username}";
     stateVersion = "22.11";
   };
+  programs.home-manager.enable = true;
 
   programs.git = import ./git.nix { inherit pkgs; };
-
   programs.zsh = import ./zsh.nix { inherit pkgs; };
-
   programs.neovim = import ./neovim.nix { inherit pkgs; };
-  programs.home-manager.enable = true;
 }
