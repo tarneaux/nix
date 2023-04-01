c74b09805663eeb666efedb427e3ff3b65c53697
tarneaux <tarneo@tarneo.fr>
2023-04-01 13:53:38 +0200

    Remove deprecated method from neovim config (LSP)

--- hosts/all/home-manager/neovim.nix
+++ hosts/all/home-manager/neovim.nix
@@ -70,8 +70,7 @@
     {
       plugin = nvim-cmp;
       config = ''
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+        capabilities = require('cmp_nvim_lsp').default_capabilities()
 
         local cmp = require'cmp'
         cmp.setup {
