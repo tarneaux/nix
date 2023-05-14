d2c7babaa9713f53ded82b46de5fdec22c27cde8
tarneo <tarneo@renn.es>
2023-05-14 12:45:02 +0200

    Remove unneeded swap device config

--- hosts/server/hardware-configuration.nix
+++ hosts/server/hardware-configuration.nix
@@ -24,8 +24,7 @@
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-label/NIXSWAP"; }
-    ];
+    [];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
