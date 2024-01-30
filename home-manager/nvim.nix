2db6ee76ca4cc57b300278f9b145db8740fdaa89
tarneo <tarneo@tarneo.fr>
2024-01-30 18:21:18 +0100

    HM(Nvim): Use C-space to accept cmp completion

--- home-manager/nvim.nix
+++ home-manager/nvim.nix
@@ -199,9 +199,8 @@
             mapping = {
               ["<C-d>"] = cmp.mapping.scroll_docs(-4),
               ["<C-f>"] = cmp.mapping.scroll_docs(4),
-              ["<C-Space>"] = cmp.mapping.complete(),
               ["<C-e>"] = cmp.mapping.close(),
-              ["<C-a>"] = cmp.mapping.confirm({
+              ["<C-space>"] = cmp.mapping.confirm({
                 behavior = cmp.ConfirmBehavior.Replace,
                 select = true,
               }),
