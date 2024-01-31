515f2496f77f48b58cd85ed614fce174bfd649d0
tarneo <tarneo@tarneo.fr>
2024-01-31 10:57:22 +0100

    OS(framy): Use own dmenu build

--- nixos/framy/configuration.nix
+++ nixos/framy/configuration.nix
@@ -188,7 +188,14 @@
         blueberry
         brightnessctl
         digikam
-        dmenu
+        (dmenu.overrideAttrs {
+          src = fetchFromGitHub {
+            owner = "tarneaux";
+            repo = "dmenu";
+            rev = "bab46f0c9f1e8dfc9e19ad26d06bcf3db76f174a";
+            sha256 = "sha256-mLViMJlTKw3UXuofFB9LLtVj/vUn+Wp+ZZivbB+Rifk=";
+          };
+        })
         element-desktop
         i3lock
         imagemagick
