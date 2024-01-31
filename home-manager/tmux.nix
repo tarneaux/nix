d39bba5a7b7b135c660dbfed6e246fada5607192
tarneo <tarneo@tarneo.fr>
2024-01-31 16:46:16 +0100

    HM(tmux): Only set prefix to C-b if user == tarneo

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -1,6 +1,17 @@
-{...}: {
+{username, ...}: {
   programs.tmux = {
     enable = true;
-    extraConfig = builtins.readFile ./config/tmux.conf;
+    extraConfig =
+      builtins.readFile ./config/tmux.conf
+      + (
+        if username == "tarneo"
+        then ''
+          # remap prefix from 'C-b' to 'C-a'
+          unbind C-b
+          set-option -g prefix C-a
+          bind-key C-a send-prefix
+        ''
+        else ""
+      );
   };
 }
