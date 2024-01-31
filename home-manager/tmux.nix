eaa3b410262d786238df0f8398a25db2ebb125cc
tarneo <tarneo@tarneo.fr>
2024-01-31 17:26:22 +0100

    HM(tmux): Simplify prefix declaration with nix opt

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -2,16 +2,7 @@
   programs.tmux = {
     enable = true;
     extraConfig =
-      builtins.readFile ./config/tmux.conf
-      + (
-        if username == "tarneo"
-        then ''
-          # remap prefix from 'C-b' to 'C-a'
-          unbind C-b
-          set-option -g prefix C-a
-          bind-key C-a send-prefix
-        ''
-        else ""
-      );
+      builtins.readFile ./config/tmux.conf;
+    prefix = (if username == "tarneo" then "C-a" else "C-b");
   };
 }
