8247ac8719c286e50af7bcdf416fa018f57af5a6
tarneo <tarneo@tarneo.fr>
2024-02-08 20:55:04 +0100

    HM(tmux): Allow creating new windows with zoxide

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -22,11 +22,43 @@
         else ""
       }
       windows=$(tmux list-windows -aF '#W (#S:#I)')
-      window=$(echo "$windows" | fzf --prompt="Switch to window: " --reverse)
-      # Get the window name from the output (in the parentheses)
-      window=$(echo "$window" | grep -oP '(?<=\().*(?=\))')
-      if [ -n "$window" ]; then
+      fzf_output=$(echo "$windows" \
+        | fzf --prompt="Switch to window: " --reverse --print-query)
+      fzf_exit_code=$?
+
+      if [ $fzf_exit_code -eq 130 ]; then
+        # The user escaped the fzf window
+        exit 0
+      fi
+
+      if [ $fzf_exit_code -eq 0 ]; then
+        # The user selected a window
+
+        # Get the second line of the output (the window name)
+        fzf_output=$(echo "$fzf_output" | sed -n 2p)
+
+        # Get the window name from the output (in the parentheses)
+        window=$(echo "$fzf_output" | grep -oP '(?<=\().*(?=\))')
+
         tmux switch-client -t "$window"
+      fi
+
+      if [ $fzf_exit_code -eq 1 ]; then
+        # The user entered a window name that doesn't exist.
+        # We can interpret it as the project directory name.
+        directory=$(zoxide query "$fzf_output")
+        if [ -n "$directory" ]; then
+          # Open a new session in the directory, with the directory name as the
+          # session name
+          dirname=$(basename "$directory")
+          tmux new-session -c "$directory" -s "$dirname" -d
+
+          # Find the session id of the new session
+          session_id=$(tmux list-sessions -F '#S:#{session_path}' \
+            | grep "$directory" \
+            | cut -d: -f1)
+          tmux switch-client -t "$session_id"
+        fi
       fi
     '')
   ];
