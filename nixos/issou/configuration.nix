e4a7e8c63faa7fcf6c0395d7363bd9ff6430f0d6
risitas@issou <admin@renn.es>
2024-02-09 14:23:19 +0100

    OS: Remove extra arguments in modules

--- nixos/issou/configuration.nix
+++ nixos/issou/configuration.nix
@@ -1,13 +1,6 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
+{...}: {
   imports = [
     ../common.nix
     ../servers.nix
