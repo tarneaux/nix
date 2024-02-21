abf4222cc0652678e3d60bba88beb03af274382b
tarneo <tarneo@tarneo.fr>
2024-02-21 17:06:41 +0100

    HM(git): Add some popular options

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -10,6 +10,23 @@
     extraConfig = {
       credential.helper = "store";
       init.defaultBranch = "main";
+      pull.rebase = true; # Rebase by default when pulling
+      merge.conflictstyle = "zdiff3"; # Also show the common ancestor
+      rebase.autostash = true; # Stash changes before rebasing
+      push.default = "current"; # Push to a remote branch with the same name
+      fetch.prune = true; # Prune remote branches when fetching
+      commit.verbose = true; # Show the diff when committing
+      diff.algorithm = "histogram"; # "Better" diff algorithm
+      core.excludesfile = "~/.config/git/ignore"; # Use a global .gitignore
+      branch.sort = "-committerdate"; # Sort branches by last commit date
+      url = {
+        "git@github.com:".pushInsteadOf = "https://github.com/";
+        "git@renn.es:".pushInsteadOf = "https://renn.es/";
+      };
     };
   };
+  home.file.".config/git/ignore".text = ''
+    .envrc
+    .direnv
+  '';
 }
