03b6063d5ef1a4425e5be384eed67fe89b36a5e5
tarneo <tarneo@tarneo.fr>
2024-03-25 16:55:11 +0100

    OS(gaspach): Add to k3s cluster

--- nixos/gaspacho/configuration.nix
+++ nixos/gaspacho/configuration.nix
@@ -1,6 +1,6 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{ ... }: {
+{ config, ... }: {
   imports = [
     ../common.nix
     ../servers.nix
@@ -21,6 +21,26 @@
       }
     ];
     defaultGateway.interface = "eno1";
+    firewall.allowedTCPPorts = [
+      6443 2379 2380 # K3s, etcd
+    ];
+  };
+
+  age.secrets = {
+    k3s = {
+      file = ../../secrets/k3s.age;
+      owner = "root";
+      group = "root";
+    };
+  };
+
+  services = {
+    k3s = {
+      enable = true;
+      role = "server";
+      tokenFile = config.age.secrets.k3s.path;
+      serverAddr = "https://192.168.1.151:6443";
+    };
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
