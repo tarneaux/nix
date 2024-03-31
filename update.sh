7d959f45ad31e7538cdfe5f0f63a0512544786c6
tarneo <tarneo@tarneo.fr>
2024-03-31 23:07:15 +0200

    update.sh: Use any of doas or sudo

--- update.sh
+++ update.sh
@@ -2,4 +2,15 @@
 
 nix flake update
 home-manager switch --flake .
-doas nixos-rebuild switch --flake .
+
+# See which of doas or sudo is installed
+if command -v doas &> /dev/null; then
+    PRIVESC=doas
+elif command -v sudo &> /dev/null; then
+    PRIVESC=sudo
+else
+  echo "Neither doas nor sudo is installed"
+  exit 1
+fi
+
+$PRIVESC nixos-rebuild switch --flake .
