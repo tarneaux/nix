4bd26bbdfc8ae9691d9dbc74ef3ae7fa64fda13d
tarneo <tarneo@tarneo.fr>
2024-04-26 13:14:12 +0200

    OS(servers): Add wireguard interface to VPS

--- nixos/servers/networking.nix
+++ nixos/servers/networking.nix
@@ -8,7 +8,7 @@
 
     interfaces.eth0.ipv4.addresses = [
       {
-        address = ipv4_addresses.${hostname};
+        address = ipv4_addresses.${hostname}.local;
         prefixLength = 24;
       }
     ];
@@ -27,6 +27,22 @@
     nftables = {
       enable = true;
       ruleset = builtins.readFile ./nftables.conf;
+    };
+
+    wireguard.interfaces = {
+      intra = {
+        ips = [ ipv4_addresses.${hostname}.wg ];
+        privateKeyFile = "/etc/wireguard/intra.key";
+
+        peers = [
+          {
+            publicKey = "5SFZ1w5fWJHJTkiL6LEeBUH6cPh1CFvRVIOuAFxf6k0=";
+            allowedIPs = [ "10.8.0.0/24" ];
+            endpoint = "vps.renn.es:64468";
+            persistentKeepalive = 25;
+          }
+        ];
+      };
     };
   };
 }
