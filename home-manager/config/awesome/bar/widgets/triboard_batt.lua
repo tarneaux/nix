88501db1bda0ccb1837bc6d80da039993cebb023
tarneo <tarneo@tarneo.fr>
2024-05-01 14:55:23 +0200

    HM(awm): Display Triboard battery status in bar

---
+++ home-manager/config/awesome/bar/widgets/triboard_batt.lua
@@ -0,0 +1,45 @@
+local awful = require("awful")
+local wibox = require("wibox")
+local gears = require("gears")
+local beautiful = require("beautiful")
+
+local textbox = wibox.widget.textbox()
+
+local logo = wibox.widget {
+    layout = wibox.layout.stack,
+    {
+        markup = '󰔶 ',
+        widget = wibox.widget.textbox()
+    },
+    {
+        markup = '<span foreground="' .. beautiful.background .. '">󰜥 </span>',
+        valign = 'bottom',
+        widget = wibox.widget.textbox()
+    },
+}
+
+local function daemon ()
+    awful.spawn.easy_async_with_shell('dbus-send --print-reply=literal --system --dest=org.bluez /org/bluez/hci0/dev_D3_69_8F_B1_D3_FB org.freedesktop.DBus.Properties.Get string:"org.bluez.Battery1" string:"Percentage"', function (stdout)
+        local percentage = tonumber(stdout:match("byte (%d+)"))
+        if percentage then
+            textbox:set_text(percentage .. "%")
+        else
+            textbox:set_text(" ")
+        end
+    end)
+end
+
+gears.timer {
+    timeout = 1,
+    call_now = true,
+    autostart = true,
+    callback = daemon
+}
+
+local widget = wibox.widget {
+    layout = wibox.layout.align.horizontal,
+    logo,
+    textbox,
+}
+
+return widget
