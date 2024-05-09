36b2c02daec2408c561ad4898b1710333807113a
tarneo <tarneo@tarneo.fr>
2024-05-09 18:52:43 +0200

    OS(chorizo): Disable docker

--- nixos/chorizo/configuration.nix
+++ nixos/chorizo/configuration.nix
@@ -57,6 +57,8 @@
     };
   };
 
+  virtualisation.docker.enable = false; # Override default for servers
+
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
