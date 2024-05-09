8f8f18d30f6005643d056a1ad9bdfc324e43c0c0
tarneo <tarneo@tarneo.fr>
2024-05-09 15:56:40 +0200

    OS(chorizo): Add network configuration

--- nixos/chorizo/configuration.nix
+++ nixos/chorizo/configuration.nix
@@ -1,6 +1,6 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{ modulesPath, ... }: {
+{ modulesPath, ipv4_addresses, hostname, ... }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
@@ -14,6 +14,53 @@
   };
 
   users.users.risitas.initialPassword = "changeme";
+
+  networking = {
+    usePredictableInterfaceNames = false;
+    hostName = hostname;
+    wg-quick.interfaces.intra = {
+      address = [ ipv4_addresses.${hostname}.wg ];
+      listenPort = 64468;
+      privateKeyFile = "/etc/wireguard/intra.key";
+      peers = [
+        {
+          publicKey = "dz8ILtS9JUmc9dJwrjSUZWfgwN1hjU5fM59Eb333Ojg=";
+          allowedIPs = [ "10.8.0.2/32" ];
+        }
+        {
+          publicKey = "Evh0vuKx61wJHVyeAQU+3cpKHIhqheIVRTj2Gua3OnA=";
+          allowedIPs = [ "10.8.0.3/32" ];
+        }
+      ];
+    };
+    defaultGateway = {
+      address = "51.210.176.1";
+      metric = 200;
+      interface = "eth0";
+    };
+    nameservers = [ "8.8.8.8" ];
+
+    interfaces.eth0.ipv4.addresses = [
+      {
+        address = ipv4_addresses.${hostname}.primary;
+        prefixLength = 24;
+      }
+      {
+        address = ipv4_addresses.${hostname}.secondary;
+        prefixLength = 24;
+      }
+    ];
+
+    nftables = {
+      enable = true;
+      ruleset = builtins.readFile ./nftables.conf;
+    };
+  };
+
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
