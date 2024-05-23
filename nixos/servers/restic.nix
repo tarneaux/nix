5c8e1e1833482990cb96a6e728a385eac5c949e1
tarneo <tarneo@tarneo.fr>
2024-05-23 08:48:02 +0200

    OS(servers): Make restic on servers a module

--- nixos/servers/restic.nix
+++ nixos/servers/restic.nix
@@ -1,29 +1,57 @@
-{ hostname, config, ... }:
+{ hostname, config, lib, ... }:
 {
-  age.secrets = {
-    "restic/${hostname}/env".file = ../../secrets/restic/${hostname}/env.age;
-    "restic/${hostname}/repo".file = ../../secrets/restic/${hostname}/repo.age;
-    "restic/${hostname}/password".file = ../../secrets/restic/${hostname}/password.age;
+  options = {
+    # Option intentionally have no default value set so that they have to be set
+    # correctly when adding a new server.
+    custom.restic.enable = lib.mkOption {
+      type = lib.types.bool;
+      description = ''
+        Whether or not to enable backups to Backblaze B2 using Restic.
+      '';
+    };
+    custom.restic.paths = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = ''
+        Directories to back up with restic.
+      '';
+    };
+    custom.restic.exclude = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = ''
+        Directories to exclude from restic backups. Syntax can be found in
+        restic's documentation, but you most likely want to use the absolute
+        path of the directory you want to exclude.
+      '';
+    };
   };
 
-  services.restic.backups = {
-    daily = {
-      initialize = true;
-      environmentFile = config.age.secrets."restic/${hostname}/env".path;
-      repositoryFile = config.age.secrets."restic/${hostname}/repo".path;
-      passwordFile = config.age.secrets."restic/${hostname}/password".path;
+  config = {
+    age.secrets = {
+      "restic/${hostname}/env".file = ../../secrets/restic/${hostname}/env.age;
+      "restic/${hostname}/repo".file = ../../secrets/restic/${hostname}/repo.age;
+      "restic/${hostname}/password".file = ../../secrets/restic/${hostname}/password.age;
+    };
 
-      paths = [
-        "/data"
-        "/hdd/data"
-        "/home/risitas/services/"
-      ];
+    services.restic.backups = lib.mkIf config.custom.restic.enable {
+      daily = {
+        initialize = true;
+        environmentFile = config.age.secrets."restic/${hostname}/env".path;
+        repositoryFile = config.age.secrets."restic/${hostname}/repo".path;
+        passwordFile = config.age.secrets."restic/${hostname}/password".path;
 
-      pruneOpts = [
-        "--keep-daily 7"
-        "--keep-weekly 5"
-        "--keep-monthly 12"
-      ];
+        paths = config.custom.restic.paths;
+        exclude = config.custom.restic.exclude;
+
+        extraBackupArgs = [
+          "--verbose=2"
+        ];
+
+        pruneOpts = [
+          "--keep-daily 7"
+          "--keep-weekly 5"
+          "--keep-monthly 12"
+        ];
+      };
     };
   };
 }
