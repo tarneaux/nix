643be2af978b2fcf759003ab42eb4e3e09fb1ca6
tarneo <tarneo@tarneo.fr>
2024-05-29 19:47:26 +0200

    HM(zsh): Use fzf for shell completion

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -2,7 +2,11 @@
   imports = [
     ./lazygit.nix
   ];
-  programs.fzf.enable = true;
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    tmux.enableShellIntegration = true;
+  };
   home.packages = with pkgs; [
     gnumake
   ];
