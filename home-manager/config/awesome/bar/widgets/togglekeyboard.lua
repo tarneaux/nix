517bf1d4268432cc0b3a5f0c6101693be765411b
tarneo <tarneo@tarneo.fr>
2024-06-13 22:47:38 +0200

    HM(awm): Make togglekeyboard widget work again

--- home-manager/config/awesome/bar/widgets/togglekeyboard.lua
+++ home-manager/config/awesome/bar/widgets/togglekeyboard.lua
@@ -3,7 +3,7 @@
 -- Kanata (which rebinds some keys) creates a new virtual device. Since I just
 -- have it enabled on the internal keyboard, I can just disable the kanata
 -- virtual device to effectively block the internal keyboard.
-local internal_keyboard = "keyboard:kanata"
+local internal_keyboard = "kanata"
 
 local awful = require("awful")
 local wibox = require("wibox")
@@ -11,8 +11,10 @@ local gears = require("gears")
 
 local widget = wibox.widget.textbox()
 
+local id = '$(xinput list | grep "' .. internal_keyboard .. '" | grep -E "(floating slave|keyboard)" | sed -n "s/.*id=\\([0-9]*\\).*/\\1/p")'
+
 local function get_enabled (callback)
-    awful.spawn.easy_async_with_shell('xinput list-props "' .. internal_keyboard ..  '" | grep "Device Enabled" | awk \'{print $4}\'', function (stdout, stderr)
+    awful.spawn.easy_async_with_shell('xinput list-props ' .. id ..  ' | grep "Device Enabled" | awk \'{print $4}\'', function (stdout)
         callback(stdout == "1\n")
     end)
 end
@@ -36,7 +38,7 @@ local function toggle ()
             new = 1
         end
 
-        awful.spawn.easy_async_with_shell('xinput set-prop "' .. internal_keyboard .. '" "Device Enabled" ' .. new, function()
+        awful.spawn.easy_async_with_shell('xinput set-prop ' .. id .. ' "Device Enabled" ' .. new, function()
             daemon()
             awful.spawn.easy_async_with_shell("~/.config/scripts/manage-keyboards")
         end)
