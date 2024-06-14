2e20a531173158902bd662f8f376149c6737316e
tarneo <tarneo@tarneo.fr>
2024-06-14 14:54:25 +0200

    OS(gaspacho): Add custom.* options (including net)

--- nixos/gaspacho/configuration.nix
+++ nixos/gaspacho/configuration.nix
@@ -10,6 +10,14 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "btrfs" ];
 
+  custom = {
+    restic.enable = false;
+    networking.ipv4 = {
+      lan = "192.168.1.153";
+      intra = "10.8.0.4/32";
+    };
+  };
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
 }
