cb150905f09c718051b7ead664aa7c66d3e363c3
tarneo <tarneo@tarneo.fr>
2024-06-14 14:32:46 +0200

    secrets(chankla): Update ssh key after reinstall

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  chankla = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPmSbq63+rfwuwqnwKot6tTk3qd9cf1LO8TUwArADwDV root@chankla";
+  chankla = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILnDleigA3arOdYGkTegPI22V+weg++/UaneiIHsIFPq root@chankla";
   issou = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGg91bCgrnL9J8WZ2L2M9XlCWxYnyQoV0HdWKlKMd5xI root@issou";
 in
 {
