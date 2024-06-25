0c9280b5e8d431a98926e4db01b7cf0f8ea10305
tarneo <tarneo@tarneo.fr>
2024-06-25 20:45:08 +0200

    HM(awm): Move powermenu.sh into awesome.nix

--- home-manager/awesome.nix
+++ home-manager/awesome.nix
@@ -63,7 +63,16 @@
     })
     (pkgs.writeShellApplication {
       name = "powermenu";
-      text = builtins.readFile ./config/powermenu.sh;
+      text = ''
+        action=$(printf 'suspend\nhibernate\nreboot\nshutdown' | dmenu)
+
+        case $action in
+            suspend) systemctl suspend;;
+            hibernate) systemctl hibernate;;
+            reboot) reboot;;
+            shutdown) shutdown now;;
+        esac
+      '';
     })
     (pkgs.writeShellApplication {
       name = "awesomewm-autostart";
