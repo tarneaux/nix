3faab74da430d664b6f781e4d5e580461eb2d20a
tarneo <tarneo@tarneo.fr>
2024-09-08 19:17:38 +0200

    HM(tarneo): Add xdg mime apps configuration

--- home-manager/tarneo.nix
+++ home-manager/tarneo.nix
@@ -106,4 +106,13 @@
     pcmanfm
     librewolf
   ];
+
+  xdg.mimeApps.enable = true;
+  xdg.mimeApps.defaultApplications = {
+    "text/html" = [ "org.qutebrowser.qutebrowser.desktop" ];
+    "x-scheme-handler/http" = [ "org.qutebrowser.qutebrowser.desktop" ];
+    "x-scheme-handler/https" = [ "org.qutebrowser.qutebrowser.desktop" ];
+    "x-scheme-handler/about" = [ "org.qutebrowser.qutebrowser.desktop" ];
+    "application/pdf" = [ "org.pwmt.zathura.desktop" ];
+  };
 }
