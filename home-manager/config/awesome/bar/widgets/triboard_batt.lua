57edddf4e045d5117680797da3edb0a169de495b
tarneo <tarneo@tarneo.fr>
2024-09-09 12:05:47 +0200

    HM(awm): Set repeat rate again on triboard conn

--- home-manager/config/awesome/bar/widgets/triboard_batt.lua
+++ home-manager/config/awesome/bar/widgets/triboard_batt.lua
@@ -18,13 +18,20 @@ local logo = wibox.widget {
     },
 }
 
+local connected = false
+
 local function daemon ()
     awful.spawn.easy_async_with_shell('dbus-send --print-reply=literal --system --dest=org.bluez /org/bluez/hci0/dev_D3_69_8F_B1_D3_FB org.freedesktop.DBus.Properties.Get string:"org.bluez.Battery1" string:"Percentage"', function (stdout)
         local percentage = tonumber(stdout:match("byte (%d+)"))
         if percentage then
             textbox:set_text(percentage .. "%")
+            if connected == false then
+                awful.spawn.with_shell("xset r rate 300 50")
+            end
+            connected = true
         else
             textbox:set_text(" ")
+            connected = false
         end
     end)
 end
