1f5248d5cc777e9059fed529bdcadf8a8cd0f85b
tarneo <tarneo@tarneo.fr>
2024-11-22 15:16:19 +0100

    OS(framy): Remove adb

--- nixos/framy/configuration.nix
+++ nixos/framy/configuration.nix
@@ -114,8 +114,6 @@
     "application/pdf" = "org.pwmt.zathura.desktop";
   };
 
-  programs.adb.enable = true;
-
   fonts.packages = with pkgs; [
     # fantasque-sans-mono
     (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
