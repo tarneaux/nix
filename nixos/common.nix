ebeddd5a9cb8f58e04a923e49ffb3dd57ead69cb
tarneo <tarneo@tarneo.fr>
2025-01-07 01:18:59 +0100

    OS(common): Move boring config to boring.nix

--- nixos/common.nix
+++ nixos/common.nix
@@ -1,12 +1,12 @@
 {
   pkgs,
-  outputs,
-  inputs,
-  lib,
-  config,
   ...
 }:
 {
+  imports = [
+    ./boring.nix
+  ];
+
   nixpkgs.config.allowUnfree = true;
 
   programs = {
@@ -49,33 +49,6 @@
 
   nix.settings.allowed-users = [ "@wheel" ];
 
-  # ############################ #
-  #  Boring flake configuration  #
-  # ############################ #
-  nixpkgs = {
-    overlays = [
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-    ];
-  };
 
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; })) (
-    (lib.filterAttrs (_: lib.isType "flake")) inputs
-  );
 
-  # Add inputs to the system's legacy channels, to make legacy nix commands
-  # consistent with flakes
-  nix.nixPath = [ "/etc/nix/path" ];
-  environment.etc = lib.mapAttrs' (name: value: {
-    name = "nix/path/${name}";
-    value.source = value.flake;
-  }) config.nix.registry;
-
-  nix.settings = {
-    experimental-features = "nix-command flakes";
-    auto-optimise-store = true;
-  };
 }
