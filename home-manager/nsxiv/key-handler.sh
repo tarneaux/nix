0ba2b6b674072b38b044e30e2cfbbd19168b3ade
tarneo <tarneo@tarneo.fr>
2025-01-17 14:59:06 +0100

    HM(nsxiv): Move key handler to its own file

---
+++ home-manager/nsxiv/key-handler.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+while read file; do
+case "$1" in
+  "y")
+    # Copy the absolute path to the clipboard.
+    # Will select the last one if multiple are selected.
+    # This has the side-effect of adding a dot in the middle of the
+    # path, but it's not a big deal since it evaluates to the same path.
+    echo -n "$PWD/$file" | xclip -selection clipboard ;;
+  "Y")
+    # Copy the image ITSELF to the clipboard.
+    # Will select the last one if multiple are selected.
+    xclip -selection clipboard -t image/png -i "$file" ;;
+  "p")
+    echo "$file" ;;
+  "r")
+    convert -rotate 90 "$file" "$file" ;;
+  "R")
+    convert -rotate -90 "$file" "$file" ;;
+  esac
+done
