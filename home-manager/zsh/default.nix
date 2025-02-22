6335fe22958aa36e053cfb228cbeca3bbb9aeb6e
tarneo <tarneo@tarneo.fr>
2025-02-22 22:13:45 +0100

    HM(zsh): Replace some unstable packages with their now stable versions

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -38,10 +38,10 @@ in
     shellAliases =
       {
         # Use eza for listing files
-        ls = "${pkgs.unstable.eza}/bin/eza --icons --group-directories-first";
+        ls = "${pkgs.eza}/bin/eza --icons --group-directories-first";
 
         # Use less as man & bat pagers
-        man = "${pkgs.unstable.bat-extras.batman}/bin/batman";
+        man = "${pkgs.bat-extras.batman}/bin/batman";
       }
       // lib.attrsets.optionalAttrs (!is_server) {
         # Reload wifi kernel module, useful when wifi doesn't work after resume
