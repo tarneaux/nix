ac292bedcf27a0e310abd209ff2183f1996c4473
tarneo <tarneo@tarneo.fr>
2025-06-05 15:37:33 +0200

    HM(nvim): Better Telescope theme without borders

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -327,6 +327,33 @@
                 },
               },
             }
+
+            local colors = require('gruvbox').palette
+            local TelescopeColor = {
+              TelescopeMatching = { fg = colors.bright_yellow },
+              TelescopeSelection = { fg = colors.light0, bg = colors.dark1, bold = true },
+
+              TelescopeNormal = { bg = colors.dark1 },
+              TelescopePromptPrefix = { link = "TelescopeNormal" },
+              TelescopePromptNormal = { link = "TelescopeNormal" },
+              TelescopeResultsNormal = { link = "TelescopeNormal" },
+              TelescopePreviewNormal = { link = "TelescopeNormal" },
+
+              TelescopeBorder = { bg = colors.dark1, fg = colors.dark1 },
+              TelescopePromptBorder = { link = "TelescopeBorder" },
+              TelescopeResultsBorder = { link = "TelescopeBorder" },
+              TelescopePreviewBorder = { link = "TelescopeBorder" },
+
+              TelescopeTitle = { bg = colors.light4, fg = colors.dark0 },
+              TelescopeResultsTitle = { link = "TelescopeTitle" },
+              TelescopePreviewTitle = { link = "TelescopeTitle" },
+              TelescopePromptTitle = { bg = colors.neutral_yellow, fg = colors.dark0, bold = true },
+            }
+
+            for hl, col in pairs(TelescopeColor) do
+              vim.api.nvim_set_hl(0, hl, col)
+            end
+
             require('which-key').add({
                 { "<leader>f", group = "Find" },
                 { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
