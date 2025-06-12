ec68b388cc0c26e94265b8f3838d65c03170baad
tarneo <tarneo@tarneo.fr>
2025-06-12 02:09:28 +0200

    HM(tmux): Much better tmw with direct argument

--- home-manager/tmux/tmw.sh
+++ home-manager/tmux/tmw.sh
@@ -6,40 +6,69 @@ else
   ATTACH_CMD="attach"
 fi
 
-windows=$(tmux list-windows -aF '#W (#S:#I)' | grep -v "fzf-window (fzf-window:0)")
-fzf_output=$(echo "$windows" \
-    | fzf --prompt="Switch to window: " --reverse --print-query)
+windows=$(tmux list-windows -aF '#S:#{session_path}' | sort | uniq | grep -v "^fzf-window:" | sed "s|^\([^=]*\):$HOME|\1:~|g")
 
-fzf_exit_code=$?
+case $# in
+    0)
+        fzf_output=$(fzf --prompt="Switch to window: " --reverse --print-query --exact <<< "$windows")
 
-if [ $fzf_exit_code -eq 130 ]; then
-    # The user escaped the fzf window
-    exit 1
-fi
+        case $? in
+            0)
+                CREATE=false
+                # Get the second line of output before colon (the window name)
+                WINDOW=$(sed -n 2p <<< "$fzf_output" | cut -d: -f1)
+                ;;
+            1)
+                CREATE=true
+                ZOXIDE_DIR="$fzf_output"
+                ;;
+            130)
+                # The user escaped the fzf window
+                exit 1
+                ;;
+            *)
+                echo "Unhandled fzf exit code."
+                exit 1
+                ;;
+        esac
+        ;;
+    1)
+        grep_output=$(grep -F "$1" <<< "$windows")
+        case $? in
+            0)
+                case $(wc -l <<< "$grep_output") in
+                    1)
+                        CREATE=false
+                        WINDOW=$(cut -d: -f1 <<< "$grep_output")
+                        ;;
+                    *)
+                        $0
+                        ;;
+                esac
+                ;;
+            1)
+                CREATE=true
+                ZOXIDE_DIR="$1"
+                ;;
+            *)
+                echo "Unhandled grep exit code."
+                exit 1
+                ;;
+        esac
+        ;;
+    *)
+        echo "Unhandled arg count."
+        exit 1
+        ;;
+esac
 
-if [ $fzf_exit_code -eq 0 ]; then
-    # The user selected a window
 
-    # Get the second line of the output (the window name)
-    fzf_output=$(echo "$fzf_output" | sed -n 2p)
-
-    # Get the window name from the output (in the parentheses)
-    window=$(echo "$fzf_output" | grep -oP '(?<=\().*(?=\))')
-
-    tmux "$ATTACH_CMD" -t "$window"
-fi
-
-if [ $fzf_exit_code -eq 1 ]; then
-    # The user entered a window name that doesn't exist.
-    # We can interpret it as the project directory name.
-    
-    # Let's first remove the leading plus sign if there one; this allows us to
-    # force the creation of a new session if results are found when searching
-    # for the directory name.
-    fzf_output=$(echo "$fzf_output" | sed -s 's/^\+//g')
-
+if [ "$CREATE" = true ]; then
+    # Remove a hypothetical leading plus sign, to allow forcing the
+    # creation of a new window.
+    dir=$(sed -s 's/^\+//g' <<< "$ZOXIDE_DIR")
     # Get the directory name with zoxide
-    directory=$(zoxide query "$fzf_output")
+    directory=$(zoxide query "$dir")
 
     if [ -z "$directory" ]; then
         echo "Couldn't query this directory's location with zoxide."
@@ -59,4 +88,6 @@ if [ $fzf_exit_code -eq 1 ]; then
         | cut -d: -f1)
 
     tmux "$ATTACH_CMD" -t "$session_id"
+else
+    tmux "$ATTACH_CMD" -t "$WINDOW"
 fi
