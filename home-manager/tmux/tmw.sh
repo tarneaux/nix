f9d9fb00f33c741435823107eb688bb121ed7477
tarneo <tarneo@tarneo.fr>
2025-06-13 16:41:57 +0200

    HM(tmux): Greatly simplify tmw script

--- home-manager/tmux/tmw.sh
+++ home-manager/tmux/tmw.sh
@@ -1,93 +1,60 @@
 #!/usr/bin/env bash
 
-if [[ -n $TMUX ]]; then
-  ATTACH_CMD="switch-client"
-else
-  ATTACH_CMD="attach"
+windows=$(tmux list-windows -aF '#S:#{session_path}' | sort | uniq | sed "s|^\([^=]*\):$HOME|\1:~|g")
+
+if [ $# -gt 1 ]; then
+    echo "Only one argument is allowed."
 fi
 
-windows=$(tmux list-windows -aF '#S:#{session_path}' | sort | uniq | grep -v "^fzf-window:" | sed "s|^\([^=]*\):$HOME|\1:~|g")
+fzf_output=$(fzf \
+    ${1+-1} -0 -e --print-query \
+    --border-label=" Tmux sessions " \
+    --tmux 40%,70% --border sharp --reverse \
+    -q "$1" <<< "$windows")
 
-case $# in
+case $? in
     0)
-        fzf_output=$(fzf --prompt="Switch to window: " --reverse --print-query --exact <<< "$windows")
-
-        case $? in
-            0)
-                CREATE=false
-                # Get the second line of output before colon (the window name)
-                WINDOW=$(sed -n 2p <<< "$fzf_output" | cut -d: -f1)
-                ;;
-            1)
-                CREATE=true
-                ZOXIDE_DIR="$fzf_output"
-                ;;
-            130)
-                # The user escaped the fzf window
-                exit 1
-                ;;
-            *)
-                echo "Unhandled fzf exit code."
-                exit 1
-                ;;
-        esac
+        # Window line is in the second line of output (because of
+        # --print-query) before the colon
+        window=$(sed -n 2p <<< "$fzf_output" | cut -d: -f1)
         ;;
     1)
-        grep_output=$(grep -F "$1" <<< "$windows")
-        case $? in
-            0)
-                case $(wc -l <<< "$grep_output") in
-                    1)
-                        CREATE=false
-                        WINDOW=$(cut -d: -f1 <<< "$grep_output")
-                        ;;
-                    *)
-                        exec $0
-                        ;;
-                esac
-                ;;
-            1)
-                CREATE=true
-                ZOXIDE_DIR="$1"
-                ;;
-            *)
-                echo "Unhandled grep exit code."
-                exit 1
-                ;;
-        esac
+        # Remove a hypothetical leading plus sign, to allow forcing the
+        # creation of a new window.
+        zoxide_dir="$(sed -s 's/^\+//g' <<< "$fzf_output")"
+
+        # Get the directory name with zoxide
+        directory=$(zoxide query "$zoxide_dir")
+
+        if [ -z "$directory" ]; then
+            echo "Couldn't query this directory's location with zoxide."
+            exit 1
+        fi
+
+        # Open a new session in the directory, with the directory name as the
+        # session name
+        dirname=$(basename "$directory")
+
+        tmux new-session -c "$directory" -s "$dirname" -d
+
+        # Find the session id of the new session (needed if there are special
+        # characters in the name)
+        window=$(tmux list-sessions -F '#S:#{session_path}' \
+            | grep ":$directory$" \
+            | cut -d: -f1)
+        ;;
+    130)
+        # The user escaped the fzf window
+        exit 1
         ;;
     *)
-        echo "Unhandled arg count."
+        echo "Unhandled fzf exit code."
         exit 1
         ;;
 esac
 
-
-if [ "$CREATE" = true ]; then
-    # Remove a hypothetical leading plus sign, to allow forcing the
-    # creation of a new window.
-    dir=$(sed -s 's/^\+//g' <<< "$ZOXIDE_DIR")
-    # Get the directory name with zoxide
-    directory=$(zoxide query "$dir")
-
-    if [ -z "$directory" ]; then
-        echo "Couldn't query this directory's location with zoxide."
-        exit 1
-    fi
-
-    # Open a new session in the directory, with the directory name as the
-    # session name
-    dirname=$(basename "$directory")
-
-    tmux new-session -c "$directory" -s "$dirname" -d
-
-    # Find the session id of the new session (needed if there are special
-    # characters in the name)
-    session_id=$(tmux list-sessions -F '#S:#{session_path}' \
-        | grep ":$directory$" \
-        | cut -d: -f1)
-
-    tmux "$ATTACH_CMD" -t "$session_id"
+if [[ -n $TMUX ]]; then
+    tmux switch-client -t "$window"
 else
-    tmux "$ATTACH_CMD" -t "$WINDOW"
+    tmux attach -t "$window"
 fi
