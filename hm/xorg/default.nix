a02708075c52a26392ab6048d4cd79b53cd217f3
tarneo <tarneo@tarneo.fr>
2025-09-05 19:25:43 +0200

    HM(xorg): Reload qprofile-based qb on autorandr switch

--- hm/xorg/default.nix
+++ hm/xorg/default.nix
@@ -67,7 +67,7 @@
         ''
           reload() {
             pkill awesome --signal HUP
-            pkill -f "^/nix/store/[[:alnum:]]*-python3-[0-9.]*/bin/python3.[0-9]* /nix/store/[[:alnum:]]*-qutebrowser-[0-9.]*/bin/.qutebrowser-wrapped$" --signal HUP
+            pkill -f "^/nix/store/[[:alnum:]]*-python3-[0-9.]*/bin/python3.[0-9]* /nix/store/[[:alnum:]]*-qutebrowser-[0-9.]*/bin/.qutebrowser-wrapped" --signal HUP
             xinput enable "AT Translated Set 2 keyboard"
           }
 
