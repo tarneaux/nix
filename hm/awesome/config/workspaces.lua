581bc32353773b92da8dcc1a7e9c2475e8c33e11
tarneo <tarneo@tarneo.fr>
2026-01-11 22:40:55 +0100

    HM(awm): Reset last workspace tag to 1 when empty

--- hm/awesome/config/workspaces.lua
+++ hm/awesome/config/workspaces.lua
@@ -20,17 +20,21 @@ workspaces.get_workspace_tags = function(screen)
 	return workspaces.get_tag_table(screen)[workspaces.current(screen)]
 end
 
+local function initialize_tpw_table_for_ws(screen, workspace_name)
+	screen.last_tag_per_workspace[workspace_name] = "1"
+
+	for tk, tv in pairs(workspaces.get_tag_table(screen)[workspace_name]) do
+		if #tv:clients() > 0 then
+			screen.last_tag_per_workspace[workspace_name] = tk
+			break
+		end
+	end
+end
+
 local function initialize_tpw_table(screen)
 	screen.last_tag_per_workspace = {}
-	for wk, wv in pairs(workspaces.get_tag_table(screen)) do
-		screen.last_tag_per_workspace[wk] = "1"
-
-		for tk, tv in pairs(wv) do
-			if #tv:clients() > 0 then
-				screen.last_tag_per_workspace[wk] = tk
-				break
-			end
-		end
+	for wk, _ in pairs(workspaces.get_tag_table(screen)) do
+		initialize_tpw_table_for_ws(screen, wk)
 	end
 end
 
@@ -39,7 +43,11 @@ workspaces.get_last_tag_of_ws = function(screen, ws)
 		initialize_tpw_table(screen)
 	end
 
-	screen.last_tag_per_workspace[workspaces.current(screen)] = workspaces.current_tag_clean(screen)
+	if #screen.selected_tag:clients() > 0 then
+		screen.last_tag_per_workspace[workspaces.current(screen)] = workspaces.current_tag_clean(screen)
+	else
+		initialize_tpw_table_for_ws(screen, workspaces.current(screen))
+	end
 
 	return workspaces.get_tag_table(screen)[ws][screen.last_tag_per_workspace[ws]]
 end
