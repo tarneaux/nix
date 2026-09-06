f8d70f43c5b1c6441222776aad92fe6dbcc0c7b0
tarneo <tarneo@tarneo.fr>
2026-09-06 11:44:52 +0200

    HM(ssh): Adapt to jump host and remove AUR

--- hm/ssh/default.nix
+++ hm/ssh/default.nix
@@ -15,30 +15,27 @@
         "ServerAliveInterval" = "60";
         "ServerAliveCountMax" = "10";
       };
-      "chorizo issou issou-lan chankla chankla-lan gaspacho" = {
-        user = "risitas";
+      "issou issou-lan chankla chankla-lan" = {
         forwardAgent = true;
         RemoteForward = {
           bind.address = "/run/user/1000/gnupg/S.gpg-agent";
           host.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
         };
       };
-      "chorizo issou chankla gaspacho" = {
-        proxyCommand = "sh -c 'tunnel check && wgx nc %h %p'";
+      "issou issou-lan chankla chankla-lan paella" = {
+        user = "risitas";
       };
-      "chorizo" = {
-        hostname = "10.8.0.1";
+      "issou chankla" = {
+        proxyJump = "jolly@paella";
+      };
+      "paella" = {
+        hostname = "51.210.247.8";
         port = 52865;
       };
-      "issou".hostname = "10.8.0.3";
+      "issou".hostname = "10.10.0.3";
       "issou-lan".hostname = "192.168.1.150";
-      "chankla".hostname = "10.8.0.2";
+      "chankla".hostname = "10.10.0.2";
       "chankla-lan".hostname = "192.168.1.151";
-      "gaspacho".hostname = "192.168.1.153";
-      "aur" = {
-        hostname = "aur.archlinux.org";
-        user = "aur";
-      };
       "github.com renn.es git.sr.ht" = {
         "ControlPersist" = "1h";
       };
